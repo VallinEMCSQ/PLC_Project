4d51effccde727f5c0ec64f73b568c79ffc9125a
adamstaudt5 <adam.staudt@ufl.edu>
2024-02-17 11:56:10 -0500

    Comparison and logical expression done, I think

--- src/main/java/plc/project/Lexer.java
+++ src/main/java/plc/project/Lexer.java
@@ -141,6 +141,7 @@ public final class Lexer {
                 match("\\\\");
             }
             throw new ParseException("Unterminated String", chars.index);
+
         }
         return chars.emit(Token.Type.STRING);
     }
