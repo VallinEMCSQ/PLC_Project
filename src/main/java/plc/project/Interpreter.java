0476092038f6939047fe31500213b1a04db8a270
VallinEMCSQ <vallinc@ufl.edu>
2024-03-21 19:40:02 -0400

    16/36 test cases passed

--- src/main/java/plc/project/Interpreter.java
+++ src/main/java/plc/project/Interpreter.java
@@ -5,6 +5,7 @@ import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
@@ -25,27 +26,90 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
 
     @Override
     public Environment.PlcObject visit(Ast.Source ast) {
-        throw new UnsupportedOperationException(); //TODO
+        // Visit all the global variables
+        ast.getGlobals().forEach(this::visit);
+
+        // Visit all the functions
+        ast.getFunctions().forEach(this::visit);
+
+        // Try to call the main/0 function
+        try {
+            // Lookup the main/0 function
+            Environment.Function mainFunction = scope.lookupFunction("main", 0);
+
+            // Prepare the arguments for the function
+            List<Environment.PlcObject> arguments = new ArrayList<>();
+
+            // Invoke the function and return its result
+            Environment.PlcObject result = mainFunction.invoke(arguments);
+
+            // Check if the result is 0
+            if (result.getValue() instanceof BigInteger && ((BigInteger) result.getValue()).intValue() == 0) {
+                return result;
+            } else {
+                throw new RuntimeException("The main/0 function does not return 0.");
+            }
+        } catch (Exception e) {
+            // The main/0 function does not exist, throw an exception
+            throw new RuntimeException("The main/0 function does not exist within source.");
+        }
     }
 
     @Override
     public Environment.PlcObject visit(Ast.Global ast) {
-        throw new UnsupportedOperationException(); //TODO
+        if (ast.getValue().isPresent()) {
+            scope.defineVariable(ast.getName(), ast.getMutable(), visit(ast.getValue().get()));
+        } else {
+            scope.defineVariable(ast.getName(), ast.getMutable(), Environment.NIL);
+        }
+        return Environment.NIL;
     }
 
     @Override
     public Environment.PlcObject visit(Ast.Function ast) {
-        throw new UnsupportedOperationException(); //TODO
+        scope.defineFunction(ast.getName(), ast.getParameters().size(), args -> {
+            Scope oldScope = scope;
+            scope = new Scope(oldScope);
+            for (int i = 0; i < ast.getParameters().size(); i++) {
+                scope.defineVariable(ast.getParameters().get(i), true, args.get(i));
+            }
+            try {
+                for (Ast.Statement statement : ast.getStatements()) {
+                    visit(statement);
+                }
+            } catch (Return returnValue) {
+                return returnValue.value;
+            } finally {
+                scope = oldScope;
+            }
+            return Environment.NIL;
+        });
+        return Environment.NIL;
     }
 
     @Override
     public Environment.PlcObject visit(Ast.Statement.Expression ast) {
-        throw new UnsupportedOperationException(); //TODO
+        return visit(ast.getExpression());
     }
 
     @Override
     public Environment.PlcObject visit(Ast.Statement.Declaration ast) {
-        throw new UnsupportedOperationException(); //TODO (in lecture)
+        Optional optional = ast.getValue();
+        Optional<Ast.Expression> op1 = ast.getValue();
+        Boolean present = optional.isPresent();
+
+        if (present){
+            Object object = optional.get();
+            System.out.println(object.toString());
+
+            Ast.Expression expr = (Ast.Expression) optional.get();
+            Ast.Expression expr1 = op1.get();
+
+            scope.defineVariable(ast.getName(), true, visit(expr));
+        } else {
+            scope.defineVariable(ast.getName(), true, Environment.NIL);
+        }
+        return Environment.NIL;
     }
 
     @Override
@@ -70,22 +134,35 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
 
     @Override
     public Environment.PlcObject visit(Ast.Statement.While ast) {
-        throw new UnsupportedOperationException(); //TODO (in lecture)
+        //throw new UnsupportedOperationException(); //TODO (in lecture)
+        while (requireType(Boolean.class, visit(ast.getCondition()))) {
+            try{
+                scope = new Scope(scope);
+                ast.getStatements().forEach(this::visit);
+            } finally {
+                scope = scope.getParent();
+            }
+        }
+        return Environment.NIL;
     }
 
     @Override
     public Environment.PlcObject visit(Ast.Statement.Return ast) {
-        throw new UnsupportedOperationException(); //TODO
+        Environment.PlcObject value = visit(ast.getValue());
+        throw new Return(value);
     }
 
     @Override
     public Environment.PlcObject visit(Ast.Expression.Literal ast) {
-        throw new UnsupportedOperationException(); //TODO
+        if (ast.getLiteral() == null){
+            return Environment.NIL;
+        }
+        return Environment.create(ast.getLiteral());
     }
 
     @Override
     public Environment.PlcObject visit(Ast.Expression.Group ast) {
-        throw new UnsupportedOperationException(); //TODO
+        return visit(ast.getExpression());
     }
 
     @Override
@@ -95,7 +172,14 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
 
     @Override
     public Environment.PlcObject visit(Ast.Expression.Access ast) {
-        throw new UnsupportedOperationException(); //TODO
+        // Return the value of the appropriate variable in the current scope. For a list, evaluate the offset and return the value of the appropriate offset.
+        Environment.Variable variable = scope.lookupVariable(ast.getName());
+        if (ast.getOffset().isPresent()){
+            int offset = requireType(Integer.class, visit(ast.getOffset().get()));
+            return Environment.create(requireType(List.class, variable.getValue()).get(offset));
+        } else {
+            return variable.getValue();
+        }
     }
 
     @Override
@@ -105,7 +189,16 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
 
     @Override
     public Environment.PlcObject visit(Ast.Expression.PlcList ast) {
-        throw new UnsupportedOperationException(); //TODO
+        // Create a new list
+        List<Object> list = new ArrayList<>();
+
+        // Add all the elements to the list
+        for (Ast.Expression expression : ast.getValues()) {
+            list.add(visit(expression).getValue());
+        }
+
+        // Return the list
+        return Environment.create(list);
     }
 
     /**
